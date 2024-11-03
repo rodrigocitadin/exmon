c83e7c62606e18bd4ec89f339bfbb3c97ce27432
rodrigocitadin <citadinrodrigo@gmail.com>
2024-11-03 03:12:29 -0300

    calculate player new life after move

--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -12,12 +12,28 @@ defmodule ExMon.Game.Actions do
     |> Map.get(move)
     |> Enum.random()
     |> fetch_player_to_update()
+    |> calculate_new_life()
   end
 
-  defp fetch_player_to_update(pw) when pw > 0, do: Game.turn() |> Game.fetch_player()
-  defp fetch_player_to_update(_pw), do: Game.reverse_turn() |> Game.fetch_player()
+  defp fetch_player_to_update(pw) when pw > 0 do
+    player = Game.turn() |> Game.fetch_player()
 
-  # defp calculate_new_life(life, pw) do
-  #   life
-  # end
+    {player, pw}
+  end
+
+  defp fetch_player_to_update(pw) do
+    player = Game.reverse_turn() |> Game.fetch_player()
+
+    {player, pw}
+  end
+
+  defp calculate_new_life({player, pw}) do
+    new_life = Map.get(player, :life) + pw
+
+    case new_life do
+      _ when new_life <= 0 -> Map.put(player, :life, 0)
+      _ when new_life >= 100 -> Map.put(player, :life, 100)
+      new_life -> Map.put(player, :life, new_life)
+    end
+  end
 end
