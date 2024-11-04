009d5d0d56f9f6f806e7307c504478e6be46f96a
rodrigocitadin <citadinrodrigo@gmail.com>
2024-11-04 11:27:50 -0300

    computer playing random moves

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -4,6 +4,7 @@ defmodule ExMon do
   alias ExMon.{Player, Game}
 
   @computer_name "machine"
+  @moves [:kick, :punch, :heal]
 
   defdelegate create_player(name), to: Player, as: :build
 
@@ -15,8 +16,17 @@ defmodule ExMon do
     Status.print_game_started()
   end
 
-  def make_move(:heal), do: Actions.move(:heal)
-  def make_move(:kick), do: Actions.move(:kick)
-  def make_move(:punch), do: Actions.move(:punch)
-  def make_move(move), do: Status.print_wrong_move(move)
+  def do_move(move) do
+    with {:ok, _} <- make_move(move) do
+      computer_move(Game.info())
+    end
+  end
+
+  defp make_move(:heal), do: {:ok, Actions.move(:heal)}
+  defp make_move(:kick), do: {:ok, Actions.move(:kick)}
+  defp make_move(:punch), do: {:ok, Actions.move(:punch)}
+  defp make_move(move), do: {:error, Status.print_wrong_move(move)}
+
+  defp computer_move(%{status: :continue, turn: :computer}), do: make_move(Enum.random(@moves))
+  defp computer_move(_), do: :ok
 end
