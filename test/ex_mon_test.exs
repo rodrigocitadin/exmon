3d064125d1bb65523a3f2af053d007bb76ac3ef0
rodrigocitadin <citadinrodrigo@gmail.com>
2024-11-05 14:04:17 -0300

    testing wip

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -1,8 +1,47 @@
 defmodule ExMonTest do
   use ExUnit.Case
-  doctest ExMon
 
-  test "greets the world" do
-    assert ExMon.hello() == :world
+  alias ExMon.Player
+
+  describe "create_player/1" do
+    test "create player with given name" do
+      player = ExMon.create_player("test_player")
+
+      assert %ExMon.Player{life: 100, moves: [:heal, :punch, :kick], name: "test_player"} ===
+               player
+    end
+  end
+
+  describe "start_game/1" do
+    setup do
+      player = Player.build("test_player")
+
+      [player: player]
+    end
+
+    test "start game with given player", context do
+      assert :ok = ExMon.start_game(context[:player])
+    end
+  end
+
+  describe "do_move/1" do
+    setup do
+      player = Player.build("test_player")
+      ExMon.start_game(player)
+
+      :ok
+    end
+
+    test "do a valid move" do
+      result = ExMon.do_move(:kick)
+
+      assert {:ok, :ok} === result
+    end
+
+    test "do a invalid move" do
+      result = ExMon.do_move(:amor)
+
+      assert :ok === result
+    end
   end
 end
